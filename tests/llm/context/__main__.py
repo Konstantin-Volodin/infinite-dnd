cc1507a035e21eb94db4a0a31022b5113a0a1f71
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2026-04-06 13:24:00 -0400

    chore: restructured tests

--- tests/llm/context/__main__.py
+++ tests/llm/context/__main__.py
@@ -1,5 +1,5 @@
-from character import main as character_context
-from dungeon_master import main as dm_context
+from tests.llm.context.character import main as character_context
+from tests.llm.context.dungeon_master import main as dm_context
 
 if __name__ == "__main__":
     character_context()
--- tests/llm/tools/__main__.py
+++ tests/llm/tools/__main__.py
@@ -1,9 +1,9 @@
-from character.perform_action import main as perform_action
-from character.speak import main as speak
-from character.travel import main as travel
-from dungeon_master.narrate import main as narrate
-from dungeon_master.create import main as create
-from dungeon_master.modify import main as modify
+from tests.llm.tools.character.perform_action import main as perform_action
+from tests.llm.tools.character.speak import main as speak
+from tests.llm.tools.character.travel import main as travel
+from tests.llm.tools.dungeon_master.narrate import main as narrate
+from tests.llm.tools.dungeon_master.create import main as create
+from tests.llm.tools.dungeon_master.modify import main as modify
 
 if __name__ == "__main__":
     perform_action()
